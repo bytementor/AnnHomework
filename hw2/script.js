74bc11591a5759fe429fe2996bd07f025e0f80b1
Anna Katunina <anna2003katunina@gmail.com>
2021-06-25 18:16:30 +0300

    add hw2

---
+++ hw2/script.js
@@ -0,0 +1,23 @@
+// Variables
+
+let userAge = 26;
+console.log('userAge', userAge);
+
+let userName = 'John';
+console.log('userName', userName);
+
+let userLastName = 'Smith';
+console.log('userLastName', userLastName);
+
+let user2 = 'Hanna';
+
+let shoppingList = 'shampoo';
+
+let userOnlineOffline = 'online';
+
+let workerSalary = 1200;
+
+let cursorСoordinates;
+
+let consoleText = 'Hello World!'
+console.log(consoleText)
